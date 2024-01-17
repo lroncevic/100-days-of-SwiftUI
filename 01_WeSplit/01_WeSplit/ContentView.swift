38bae4bffbd7cf5e4f75907a4f1f72cef41e6b21
lroncevic <roncevicluka@outlook.com>
2024-01-17 11:44:34 +0100

    Add first project setup

---
+++ 01_WeSplit/01_WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01_WeSplit
+//
+//  Created by Luka Rončević on 17.01.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01_WeSplit/01_WeSplit/_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_WeSplitApp.swift
+//  01_WeSplit
+//
+//  Created by Luka Rončević on 17.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct _1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
