c4b65480f3267765255e1cf1c1fde9118ba1a838
lroncevic <roncevicluka@outlook.com>
2024-01-18 12:28:07 +0100

    Project 2 setup

---
+++ 02_GuessTheFlag/02_GuessTheFlag/_2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_GuessTheFlagApp.swift
+//  02_GuessTheFlag
+//
+//  Created by Luka Rončević on 18.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct _2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
