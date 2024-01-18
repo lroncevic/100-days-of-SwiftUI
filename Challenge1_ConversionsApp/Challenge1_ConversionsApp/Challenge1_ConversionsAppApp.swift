9daea88102242bb3432abb4ea8887d8663f3df5d
lroncevic <roncevicluka@outlook.com>
2024-01-18 09:32:18 +0100

    Add challenge project set up

---
+++ Challenge1_ConversionsApp/Challenge1_ConversionsApp/Challenge1_ConversionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1_ConversionsAppApp.swift
+//  Challenge1_ConversionsApp
+//
+//  Created by Luka Rončević on 18.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct Challenge1_ConversionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1_ConversionsApp/Challenge1_ConversionsApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Challenge1_ConversionsApp
+//
+//  Created by Luka Rončević on 18.01.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+//    @State private var inputUnit: String
+//    @State private var outputUnit: String
+//    @State private var inputNumber = 0.0
+//    @State private var outputResult = 0.0
+    
+    var body: some View {
+        NavigationStack{
+            Form {
+                Section {
+                    
+                }
+                
+            }
+            .navigationTitle("ConversionsApp")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
